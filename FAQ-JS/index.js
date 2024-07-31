bb4e1e1a817405e041103e8375f458b4f63fbdd5
kannchann <kannchann.thapa@gmail.com>
2024-07-31 12:04:07 +0545

    toggle

---
+++ FAQ-JS/index.js
@@ -0,0 +1,8 @@
+//variables
+const accordian = document.getElementsByClassName("content-container");
+
+for (i = 0; i < accordian.length; i++) {
+  accordian[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
